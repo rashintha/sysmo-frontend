67cddb59df394f42fe6bec4e3f001b1ee7845d8f
Rashintha Maduneth <rashinthamaduneth@gmail.com>
2022-12-18 11:40:24 +0530

    Login page design added. 404 design added.

---
+++ src/components/errors/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <div>
+        <span className='text-9xl text-red-900'>404</span> <span className='text-4xl text-orange-600'>NOT FOUNT</span>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
